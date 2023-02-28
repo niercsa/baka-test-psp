18d60ea8305adab5d630af34c9b2a772cc0bd164
gil-unx <57790408+gil-unx@users.noreply.github.com>
2023-02-28 07:15:08 +0000

    Add files via upload

--- decode_sc.bat
+++ decode_sc.bat
@@ -1,2 +1,2 @@
-tools\nichi\Nichi.exe 1_extracted\sc 1_extracted\txt
+tools\baka\Baka.exe 1_extracted\sc 1_extracted\txt
 pause
--- encode_sc.bat
+++ encode_sc.bat
@@ -1,2 +1,2 @@
-tools\nichi\Nichi.exe 1_extracted\sc 2_translated\txt 3_patched\sc
+tools\baka\Baka.exe 1_extracted\sc 2_translated\txt 3_patched\sc
 pause
